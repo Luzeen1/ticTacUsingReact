76d46ac25ce46a5d1826dd9b4af60cf70db068bb
Luzeen Halaby <Luzeen.Halaby@clicktale.com>
2020-05-20 18:45:04 +0300

    Updates

---
+++ precache-manifest.2b818dbc8cc98159be076d70433e53ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b9fded09ea7e8fcba934dbcd52ed88",
+    "url": "/ticTacUsingReact/index.html"
+  },
+  {
+    "revision": "1c9bccb0c8770c85353a",
+    "url": "/ticTacUsingReact/static/css/main.b7238b36.chunk.css"
+  },
+  {
+    "revision": "8aec48639db7574f80dd",
+    "url": "/ticTacUsingReact/static/js/2.9fad9b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacUsingReact/static/js/2.9fad9b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9bccb0c8770c85353a",
+    "url": "/ticTacUsingReact/static/js/main.b3e14343.chunk.js"
+  },
+  {
+    "revision": "fcf23095e180e98001de",
+    "url": "/ticTacUsingReact/static/js/runtime-main.3825b2ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ticTacUsingReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
